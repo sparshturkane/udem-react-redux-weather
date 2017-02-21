3e0c7839f8d662ef982f1b75c6112e2e6e0b7914
sparshturkane <rajaturkane99@gmail.com>
2017-02-21 07:37:17 +0530

    created new action creator responsible for ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 const API_KEY = 'd92baeb292ababcf0f13309a588136f4';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
